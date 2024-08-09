45179c92ecce32528a28418b7e637f1d49fdd3c5
amankass <137434910+amankass@users.noreply.github.com>
2024-08-09 00:58:31 -0800

    Connect to MongoDb DataBase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/Melody-App")
+  .then(() => console.log("MongoDB Connected"));
 
 app.listen(3000, () => {
   console.log("Server is Running on port 3000");
