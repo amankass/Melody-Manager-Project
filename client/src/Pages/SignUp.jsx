4691241efadb52d9f71b16c487ed3e97cf469fd8
amankass <137434910+amankass@users.noreply.github.com>
2024-08-21 11:48:05 -0800

    Add SignUp Functionality and Mkae Server Proxy

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -20,7 +20,7 @@ function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
