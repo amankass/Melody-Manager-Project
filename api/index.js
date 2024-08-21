43cdbdb7b9e7a57261f4c412e1f379343863bcc2
amankass <137434910+amankass@users.noreply.github.com>
2024-08-21 09:05:43 -0800

    Create SignUp Route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user-routes.js";
+import authRouter from "./routes/auth-routes.js";
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
   .connect("mongodb://localhost:27017/Melody-App")
@@ -13,3 +16,4 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
